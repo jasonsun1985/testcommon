1f44ad5fa9cb35825c731f14efc12b85033aecbd
sunlei4 <sunlei4@fehorizon.com>
2021-04-27 15:02:44 +0800

    update

--- src/main/java/com/tec/cas/ABADemo2.java
+++ src/main/java/com/tec/cas/ABADemo2.java
@@ -22,10 +22,10 @@ public class ABADemo2 {
                 e.printStackTrace();
             }
             //使用stamp 替代reference.getStamp() 能解决ABA问题
-            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
-            System.out.println("第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference()+"  更新结果：" + b1);
-            boolean b2 = reference.compareAndSet(101, 100, reference.getStamp(), reference.getStamp() + 1);
-            System.out.println("第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference()+"  更新结果：" + b2);
+            boolean b1 = reference.compareAndSet(100, 101, stamp, stamp + 1);
+            System.out.println("第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b1);
+            boolean b2 = reference.compareAndSet(101, 100, stamp, stamp + 1);
+            System.out.println("第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b2);
         }, "t3").start();
 
         new Thread(() -> {
@@ -37,8 +37,8 @@ public class ABADemo2 {
                 e.printStackTrace();
             }
             //由于原有值100上面已经被修改为101，所以不会修改为2019
-            boolean result = reference.compareAndSet(101, 2019, reference.getStamp(), reference.getStamp() + 1);
-            System.out.println("版本号："+reference.getStamp());
+            boolean result = reference.compareAndSet(101, 2019, stamp, stamp + 1);
+            System.out.println("版本号：" + reference.getStamp());
             System.out.println(Thread.currentThread().getName() + "修改是否成功:" + result + "\t当前最新实际版本号:" + reference.getStamp() + " 值为： " + reference.getReference());
             System.out.println(Thread.currentThread().getName() + "\t当前实际最新值:" + reference.getReference() + " 值为： " + reference.getReference());
         }, "t4").start();
---
+++ src/main/java/com/tec/cas/AtomicExample.java
@@ -0,0 +1,47 @@
+package com.tec.cas;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * @author JASON
+ * @version 1.0
+ * @Description
+ * @date 2021/4/27 14:45
+ */
+public class AtomicExample {
+    private static AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(99, 1);
+
+
+    public static void main(String[] args) {
+        Thread student1 = new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName() + "---首次 stamp： " + stamp);
+
+            atomicStampedReference.compareAndSet(99, 100,
+                    atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+            System.out.println(Thread.currentThread().getName() + "---第二次 stamp： " + atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(100, 99,
+                    atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+            System.out.println(Thread.currentThread().getName() + "---第三次 stamp： " + atomicStampedReference.getStamp());
+        }, "student1");
+
+        Thread student2 = new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println(Thread.currentThread().getName() + "---第一次 stamp： " + stamp);
+            try {
+                System.out.println(Thread.currentThread().getName() + "---你的校园网正在尝试重新连接......");
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean result = atomicStampedReference.compareAndSet(99, 100,
+                    stamp, stamp + 1);
+            System.out.println(Thread.currentThread().getName() + "---修改成功与否：" + result + "  当前 stamp：" + atomicStampedReference.getStamp());
+            System.out.println(Thread.currentThread().getName() + "---当前课程已选人数：" + atomicStampedReference.getReference());
+        }, "student2");
+
+        student1.start();
+        student2.start();
+    }
+}
