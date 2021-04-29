ad7fb7cc8e4d2c96b33976199035ae9187bd5cb1
sunlei4 <sunlei4@fehorizon.com>
2021-04-29 19:26:37 +0800

    update

--- src/main/java/com/tec/cas/ABADemo2.java
+++ src/main/java/com/tec/cas/ABADemo2.java
@@ -1,6 +1,9 @@
 package com.tec.cas;
 
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
@@ -10,8 +13,54 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 public class ABADemo2 {
 
     static AtomicStampedReference<Integer> reference = new AtomicStampedReference<>(100, 1);
-
-    public static void main(String[] args) {
+    static ExecutorService executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+            60L, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>(100));
+    public static void main(String[] args) throws InterruptedException {
+        /*
+        executorService.execute(()-> {
+                int stamp = reference.getStamp();
+                System.out.println("第1次版本号:" + stamp);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //使用stamp  能解决ABA问题
+                boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+                System.out.println("第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b1);
+                boolean b2 = reference.compareAndSet(101, 100, reference.getStamp(), reference.getStamp() + 1);
+                System.out.println("第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b2);
+        });
+        executorService.execute(()-> {
+            int stamp = reference.getStamp();
+            System.out.println("2-第1次版本号:" + stamp);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //使用stamp 替代reference.getStamp() 能解决ABA问题
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("2-第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b1);
+            boolean b2 = reference.compareAndSet(101, 100, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("2-第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b2);
+        });
+        executorService.execute(()-> {
+            int stamp = reference.getStamp();
+            System.out.println("3-第1次版本号:" + stamp);
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //由于原有值100上面已经被修改为101，所以不会修改为2019
+            boolean result = reference.compareAndSet(101, 2019, stamp, stamp + 1);
+            System.out.println("3-版本号：" + reference.getStamp());
+            System.out.println(Thread.currentThread().getName() + "修改是否成功:" + result + "\t当前最新实际版本号:" + reference.getStamp() + " 值为： " + reference.getReference());
+            System.out.println(Thread.currentThread().getName() + "\t当前实际最新值:" + reference.getReference() + " 值为： " + reference.getReference());
+        });
+        */
 
         new Thread(() -> {
             int stamp = reference.getStamp();
@@ -21,16 +70,29 @@ public class ABADemo2 {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //使用stamp 替代reference.getStamp() 能解决ABA问题
-            boolean b1 = reference.compareAndSet(100, 101, stamp, stamp + 1);
+            //使用stamp  能解决ABA问题
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
             System.out.println("第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b1);
-            boolean b2 = reference.compareAndSet(101, 100, stamp, stamp + 1);
+            boolean b2 = reference.compareAndSet(101, 100, reference.getStamp(), reference.getStamp() + 1);
             System.out.println("第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b2);
         }, "t3").start();
-
         new Thread(() -> {
             int stamp = reference.getStamp();
-            System.out.println("第1次版本号:" + stamp);
+            System.out.println("2-第1次版本号:" + stamp);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //使用stamp 替代reference.getStamp() 能解决ABA问题
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("2-第2次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b1);
+            boolean b2 = reference.compareAndSet(101, 100, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("2-第3次版本号:" + reference.getStamp() + " 值为： " + reference.getReference() + "  更新结果：" + b2);
+        }, "t1").start();
+        new Thread(() -> {
+            int stamp = reference.getStamp();
+            System.out.println("3-第1次版本号:" + stamp);
             try {
                 TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
@@ -38,10 +100,12 @@ public class ABADemo2 {
             }
             //由于原有值100上面已经被修改为101，所以不会修改为2019
             boolean result = reference.compareAndSet(101, 2019, stamp, stamp + 1);
-            System.out.println("版本号：" + reference.getStamp());
+            System.out.println("3-版本号：" + reference.getStamp());
             System.out.println(Thread.currentThread().getName() + "修改是否成功:" + result + "\t当前最新实际版本号:" + reference.getStamp() + " 值为： " + reference.getReference());
             System.out.println(Thread.currentThread().getName() + "\t当前实际最新值:" + reference.getReference() + " 值为： " + reference.getReference());
         }, "t4").start();
+            Thread.sleep(2000);
+        System.out.println("最终邮戳值为："+reference.getStamp());
 
     }
 }
---
+++ src/main/java/com/tec/cas/ABAPeopleDemo.java
@@ -0,0 +1,54 @@
+package com.tec.cas;
+
+
+import com.alibaba.fastjson.JSON;
+import com.tec.method.People;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * 解决ABA问题
+ */
+public class ABAPeopleDemo {
+    static final int DEFAULT_NUM = 100;
+    static final int DEFAULT_STAMP = 1;
+    static AtomicStampedReference<Integer> reference = new AtomicStampedReference<>(DEFAULT_NUM, DEFAULT_STAMP);
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            int stamp = reference.getStamp();
+            System.out.println("第1次版本号:" + stamp);
+            People p = new People();
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("T1" + b1);
+            if (b1) {
+                People p1 = new People("jason", 10);
+                System.out.println(Thread.currentThread().getName()+ JSON.toJSON(p1));
+                reference.set(DEFAULT_NUM, 1);
+            }
+        }, "T1").start();
+        new Thread(() -> {
+            People p = new People();
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("T2" + b1);
+            if (b1) {
+                People p1 = new People("jason", 10);
+                System.out.println(Thread.currentThread().getName()+ JSON.toJSON(p1));
+                reference.set(DEFAULT_NUM, 1);
+            }
+        }, "T2").start();
+        new Thread(() -> {
+            People p = new People();
+            boolean b1 = reference.compareAndSet(100, 101, reference.getStamp(), reference.getStamp() + 1);
+            System.out.println("T3" + b1);
+            if (b1) {
+                People p1 = new People("jason", 10);
+                System.out.println(Thread.currentThread().getName()+ JSON.toJSON(p1));
+                reference.set(DEFAULT_NUM, 1);
+            }
+        }, "T3").start();
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("最终信息为：" + reference.getReference() + "  " + reference.getStamp());
+    }
+}
