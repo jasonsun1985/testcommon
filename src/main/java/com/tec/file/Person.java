77565eb1f27e04328e39577f8863cedbbd1f4e2a
sunlei4 <sunlei4@fehorizon.com>
2021-05-06 22:29:19 +0800

    update

--- src/main/java/com/tec/file/Person.java
+++ src/main/java/com/tec/file/Person.java
@@ -2,11 +2,13 @@ package com.tec.file;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String name;
     private int age;
